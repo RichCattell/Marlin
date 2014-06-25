ae4866585b4340d7d9cf96fe49f4efd730aa8a58
braddo99 <braddo_99@yahoo.com>
2014-06-25 02:26:07 -0400

    PROBING_FEEDRATE define added
    PROBING_FEEDRATE will be used by G30 autocalibration, and can be changed on the fly by G30 A Fnnn

--- Configuration.h
+++ Configuration.h
@@ -108,6 +108,7 @@
 
 // Z-Probe variables
 // Start and end location values are used to deploy/retract the probe (will move from start to end and back again) 
+#define PROBING_FEEDRATE 100/10*60  // Speed for individual probe Use: G30 A F600
 #define Z_PROBE_OFFSET {0, 10, -5.6, 0}  // X, Y, Z, E distance between hotend nozzle and deployed bed leveling probe.
 #define Z_PROBE_DEPLOY_START_LOCATION {20, 96, 30, 0}   // X, Y, Z, E start location for z-probe deployment sequence
 #define Z_PROBE_DEPLOY_END_LOCATION {5, 96, 30, 0} 	  // X, Y, Z, E end location for z-probe deployment sequence
